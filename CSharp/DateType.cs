c1d8ba8622262b43c20028aa1d14a559879c9538
SWY <swy@SWYdeiMac-2.local>
2018-07-18 15:51:17 +0800

    add datetype

---
+++ CSharp/DateType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp
+{
+    partial class MainClass
+    {
+        /// <summary>
+        /// XML注释 
+        /// </summary>
+        static void Note()
+        {
+            //单行注释
+
+            /*
+             多行注释
+             */
+        }
+
+        static void NameSpace()
+        {
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+        }
+    }
+}
